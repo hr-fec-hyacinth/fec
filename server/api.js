0508a36d851fa618027c69eb434f304afa11c332
Jonathon Zimdars <zimdarsj@kenyon.edu>
2022-12-02 11:19:55 -0600

    Completion of API Helper and Splitting Up Components

---
+++ server/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import fs from 'fs';
+import AUTHKEY from './config.js';
+
+const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/'
+
+let api = {};
+api.getProduct = (productId, success = () => {}, error = () => {}) => {
+  axios.get(URL + 'products/' + productId, {headers: {Authorization: AUTHKEY}})
+    .then(res => success(res.data))
+    .catch(err => error(err));
+};
+
+api.getStyles = (productId, success = () => {}, error = () => {}) => {
+  axios.get(URL + 'products/' + productId + '/styles', {headers: {Authorization: AUTHKEY}})
+    .then(res => success(res.data))
+    .catch(err => error(err));
+};
+
+export default api;
