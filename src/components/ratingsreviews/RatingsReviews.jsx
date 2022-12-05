b35046a6c01687491df7529a89aa936728ea6277
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 14:05:27 -0500

    Modified Imports and Exports of Rating Reviews area after pulling down Jon's API

--- src/components/ratingsreviews/RatingsReviews.jsx
+++ src/components/ratingsreviews/RatingsReviews.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import Ratings from './Ratings.jsx';
+import Reviews from './Reviews.jsx';
+import RatingReviewForm from './RatingReviewForm.jsx';
 
 const RatingsReviews = ({product}) => {
+  // Retrieves Reviews of Current Product
+  // Retrieves Reviews of Product Metadata
+  const [] =
 
   return (
     <div id='ratings-reviews'>
-      Ratings & Reviews
-      <Ratings product={} />
-      <Reviews product={}/>
-      <ReviewForm product={}/>
+      RATINGS & REVIEWS
+      <Ratings product={product} />
+      <Reviews product={product}/>
+      <ReviewForm product={product}/>
     </div>
   )
 }
