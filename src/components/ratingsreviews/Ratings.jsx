12daa4562838956b17cd4b3b1d025589bae3c26f
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 16:08:20 -0500

    Made Characteristics List

--- src/components/ratingsreviews/Ratings.jsx
+++ src/components/ratingsreviews/Ratings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { calculateAverageRating } from './reviewsHelper.js';
+import CharacteristicsList from './CharacteristicsList.jsx';
 
 const Ratings = ({product, meta}) => {
   const [averageRating, setAverageRating] = useState(0);
@@ -16,6 +17,9 @@ const Ratings = ({product, meta}) => {
     <div id='ratings'>
       {averageRating && <div>{averageRating}</div>}
       {meta.product_id}
+      {meta.characteristics &&
+        <CharacteristicsList characteristics={meta.characteristics}
+      />}
     </div>
   )
 }
