d2719331928b2486fbac9db6c342e672246b679d
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 15:31:36 -0500

    Calculated average rating of products to the first decimal place

--- src/components/ratingsreviews/Ratings.jsx
+++ src/components/ratingsreviews/Ratings.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const Ratings = ({product}) => {
+const Ratings = ({product, meta}) => {
+  const [averageRating, setAverageRating] = useState(0);
+
+  // calculates Average Rating i:Object o: average num
+  const calculateAverage = (ratings) => {
+    let totalRating = 0;
+    let totalVotes = 0;
+
+    Object.keys(ratings).forEach(el => {
+      totalRating += Number(ratings[el]) * el;
+      totalVotes += Number(ratings[el]);
+    })
+
+    // return parseFloat((totalRating / totalVotes)).toFixed(2) ;
+    return (totalRating / totalVotes).toFixed(1);
+  }
+
+  // calculateAverage();
+
+  // calculates average rating upon load
+  useEffect(() => {
+    if(meta.ratings) {
+      setAverageRating(calculateAverage(meta.ratings));
+    }
+  }, [meta]);
 
   return (
     <div id='ratings'>
+      {averageRating && <div>{averageRating}</div>}
+      {meta.product_id}
     </div>
   )
 }
--- src/components/ratingsreviews/RatingsReviews.jsx
+++ src/components/ratingsreviews/RatingsReviews.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Ratings from './Ratings.jsx';
 import Reviews from './Reviews.jsx';
-import RatingReviewForm from './RatingReviewForm.jsx';
+import ReviewForm from './ReviewForm.jsx';
 
-const RatingsReviews = ({product}) => {
+const RatingsReviews = ({product, meta}) => {
   // Retrieves Reviews of Current Product
   // Retrieves Reviews of Product Metadata
-  const [] =
+  // Sets form render to false on initial load
 
+  const [renderForm, setRenderForm] = useState(false);
+
+  // ReviewForm component purposefully added in but prevented from being rendered
   return (
-    <div id='ratings-reviews'>
+    <div id='ratings-reviews' className="container mx-auto">
       RATINGS & REVIEWS
-      <Ratings product={product} />
-      <Reviews product={product}/>
-      <ReviewForm product={product}/>
+      <div className="flex flex-row">
+        <Ratings className="w-full aspect-video" product={product} meta={meta}/>
+        <Reviews className="w-full aspect-video" product={product} />
+      </div>
+      {renderForm && <ReviewForm product={product} />}
     </div>
   )
 }
