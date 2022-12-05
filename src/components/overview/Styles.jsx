e1be9fa7394103cd2df50719b0a747fc7af70d74
Jonathon Zimdars <zimdarsj@kenyon.edu>
2022-12-05 13:11:39 -0600

    Limited Thumbnails to 4 per line

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import {AiFillCheckCircle} from 'react-icons/ai';
-const Styles = ({styles, styleIndex, changeStyleIndex}) => {
+import { AiFillCheckCircle } from 'react-icons/ai';
+const Styles = ({ styles, styleIndex, changeStyleIndex }) => {
 
   return (
-    <div className='flex flex-wrap'>
-      {styles.map((style, index) => {
-        return (<div key={style.style_id} onClick={e => {
-          if (index !== styleIndex) {
-            changeStyleIndex(index);
-          }
-        }}>
-          {index === styleIndex && <div className='absolute ml-20 mt-1 bg-white rounded-full'><AiFillCheckCircle className='text-blue-400'/></div>}
-          <img className='w-28 object-cover h-28 rounded-full' src={style.photos[0].thumbnail_url}></img></div>)
-      })}
+    <div>
+      <p>Style > {styles[styleIndex] && styles[styleIndex].name}</p>
+      <div className='flex flex-wrap mx-auto'>
+        {styles.map((style, index) => {
+          return (<div key={style.style_id} className='w-3/12 p-1' onClick={e => {
+            if (index !== styleIndex) {
+              changeStyleIndex(index);
+            }
+          }}>
+            {index === styleIndex && <div className='absolute mt-1 bg-white rounded-full'><AiFillCheckCircle className='text-blue-400' /></div>}
+            <img className='aspect-square min-w-full object-cover rounded-full' src={style.photos[0].thumbnail_url}></img></div>)
+        })}
+      </div>
     </div>
   );
 };
