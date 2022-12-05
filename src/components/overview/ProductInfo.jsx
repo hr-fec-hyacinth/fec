c7acbac74d73950c03075ae95e1dfd4d52993044
Jonathon Zimdars <zimdarsj@kenyon.edu>
2022-12-05 08:59:58 -0600

    Completion of Sale Price

---
+++ src/components/overview/ProductInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const {useState, useEffect} = React;
+
+const ProductInfo = ({product, style}) => {
+  const [price, updatePrice] = useState('');
+  const [salePrice, updateSalePrice] = useState('');
+
+  // Calculate Price on style change
+  useEffect(() => {
+    updatePrice(style.original_price);
+    if (style.sale_price !== '0' && style.sale_price) {
+      updateSalePrice(style.sale_price);
+    }
+  }, [style]);
+
+  return (
+    <div>
+      Product Info
+      <p>{product.category}</p>
+      <p>{product.name}</p>
+      {!salePrice && <p>${price}</p>}
+      {salePrice && <p><span className='text-red-600 mr-2'>${salePrice}</span><span className='line-through'>${price}</span></p>}
+    </div>
+  )
+};
+
+export default ProductInfo;
