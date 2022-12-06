1fc8bf1a0a92fe98f5aadc94cccf930ea7b84bd9
nathan7432 <steelersfan5052@yahoo.com>
2022-12-06 11:12:31 -0500

    report functionality complete

--- src/components/qna/OneA.jsx
+++ src/components/qna/OneA.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const OneA = ({ answer }) => {
-  const dateString = answer.date;
-  const date = new Date(dateString);
+  const date = new Date(answer.date);
   const options = { year: "numeric", month: "long", day: "numeric" };
   const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const [helpfulness, setHelpfulness] = useState(0);
+  const [reported, setReported] = useState(false);
+
+  useEffect(() => {
+    setHelpfulness(answer.helpfulness)
+  }, [answer])
+
+  const handleYesClick = () => {
+    setHelpfulness(answer.helpfulness+1)
+  }
+
+  const handleReportClick = () => {
+    setReported(true);
+  }
+
+  if(reported) {
+    var reportComp = <div>Reported</div>
+  } else {
+    var reportComp = <div onClick={handleReportClick}>Report</div>
+  }
+
   return (
     <>
       <div>A: {answer.body}</div>
-      <div>by: {answer.answerer_name}, {formattedDate} | Helpful? | Yes ({answer.helpfulness}) | Report</div>
+      <div>by: {answer.answerer_name}, {formattedDate} | Helpful? | Yes ({helpfulness}) | Report</div>
+      <div onClick={handleYesClick}>Yes</div>
+      {reportComp}
     </>
   )
 }
