d4a9c78add9dda69a8cbeb087ae835dfb1011ada
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 22:57:56 -0500

    Decreased Vertical Size of Reviews Chart & Added Hover

--- src/components/ratingsreviews/RatingsChart.jsx
+++ src/components/ratingsreviews/RatingsChart.jsx
@@ -14,12 +14,13 @@ const RatingsChart = ({metaRatings}) => {
         const fillStyle = {
           width: `${(metaRatings[el]/totalRatings) * 100}%`,
           height: '100%',
-          backgroundColor: '#f6fa12'
+          verticalAlign: 'middle',
+          backgroundColor: '#ffbf00f0'
         }
 
         return (
-          <div key={el + i} className='flex py-1'>
-          <div className='w-3/12 text-xl'>{el + ' Star:'}</div>
+          <div key={el + i} className='flex py-1 hover:bg-sky-300'>
+          <div className='w-3/12 text-xs'>{el + ' Star:'}</div>
           <div className='w-8/12' style={{
             border: 'solid black 1px',
             backgroundColor: 'light gray'
