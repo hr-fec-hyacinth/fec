4c091c8ac72d1ebcbf4746361c45679ae89579eb
nathan7432 <steelersfan5052@yahoo.com>
2022-12-06 10:13:46 -0500

    started answer list functionality

--- src/components/qna/AList.jsx
+++ src/components/qna/AList.jsx
@@ -3,19 +3,42 @@ import OneA from './OneA.jsx'
 
 const AList = ({ answers }) => {
 
-  const [display, setDisplay] = useState([])
+  const [display, setDisplay] = useState([]);
+  const [collapse, setCollapse] = useState(false);
+  const [more, setMore] = useState(false);
 
-  useEffect(() => {
-    if (answers.length > 0) {
-      setDisplay([answers[0], answers[1]])
+  useEffect(() => { setAnswersDefault() }, [answers])
+
+  const setAnswersDefault = () => {
+    if (answers.length > 1) {
+      setDisplay([answers[0], answers[1]]);
+    } else if (answers.length === 1) {
+      setDisplay([answers[0]]);
     }
-  }, [answers])
+  }
+
+  const handleShowClick = () => {
+    setDisplay(answers);
+    setCollapse(true);
+    setMore(false);
+  }
+
+  const handleCollapseClick = () => {
+    setAnswersDefault();
+    s
+  }
+
+  const element;
+  if(display.length === 2 && answers.length > 2) { //there are two displaying and there are more
+    <div onClick={handleShowClick}>See more answers</div>
+  } else if (display.length === 1 || display) {} //there is one or two displaying and no more
 
 
   return (
-    <>
+    <div className='overflow-y-auto max-h-halfScreen'>
       {display.map(answer => <OneA answer={answer}/>)}
-    </>
+      {answers.length!==display.length && }
+    </div>
   )
 }
 
