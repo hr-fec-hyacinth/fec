58e7238ced7e69e998c099037744d72e1d30fec7
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 23:12:06 -0500

    Added Stationary Stars to Reviews

--- src/components/ratingsreviews/Reviews.jsx
+++ src/components/ratingsreviews/Reviews.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import totalReviews from '../../helper/totalReviews.js'
 import SortOptions from './SortOptions.jsx';
 
 // filterStars is selected by
@@ -11,6 +10,9 @@ const RatingsReviews = ({product, meta, filterStars}) => {
   return (
     <div id='review' className="w-8/12">
       <SortOptions meta={meta} />
+      <div id="ReviewList" className="flex h-48 bg-slate-200">
+        <p>&#9733;&#9733;&#9733;&#9733;</p>
+      </div>
     </div>
   );
 };
