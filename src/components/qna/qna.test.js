794113278515cfacce58b80b4a50c03fe724fb53
nathan7432 <steelersfan5052@yahoo.com>
2022-12-06 16:27:02 -0500

    testing env setup

--- src/components/qna/qna.test.js
+++ src/components/qna/qna.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import QnA from './QnA.jsx';
 
+let product = {
+  "id": 11,
+  "name": "Air Minis 250",
+  "slogan": "Full court support",
+  "description": "This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.",
+  "category": "Basketball Shoes",
+  "default_price": "0",
+};
+
 it('Renders the Overview Section', () => {
   const placeholder = 'QUESTIONS & ANSWERS';
-  const { getByText } = render(<QnA product={}/>); //TODO fill with hard coded api response
+  const { getByText } = render(<QnA product={product}/>);
   getByText(placeholder);
 })
