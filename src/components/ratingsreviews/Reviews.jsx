46fb765adc55bf2f3149b98f0f3b971e640a3ba5
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-05 23:41:54 -0500

    Completed most basic Static Text Mockup of the right side of the site

--- src/components/ratingsreviews/Reviews.jsx
+++ src/components/ratingsreviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import SortOptions from './SortOptions.jsx';
+import { AiFillCheckCircle } from 'react-icons/ai';
 
 // filterStars is selected by
 const RatingsReviews = ({product, meta, filterStars}) => {
@@ -10,8 +11,23 @@ const RatingsReviews = ({product, meta, filterStars}) => {
   return (
     <div id='review' className="w-8/12">
       <SortOptions meta={meta} />
-      <div id="ReviewList" className="flex h-48 bg-slate-200">
-        <p>&#9733;&#9733;&#9733;&#9733;</p>
+      <div id="ReviewList" className="h-48 bg-slate-200 mx-auto px-3">
+        <div>
+          <div>&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+          <div className="text-xs">Username, January 1, 2019</div>
+        </div>
+        <div className='font-semibold truncate'>
+          Review Title with word-break truncation to prevent wrapping onto the next...
+        </div>
+        <div className='font-light'>
+          ... line, if necessary
+        </div>
+        <div>
+          I really like these camisole pants man. Bought the last 100 on blackfriday sale.
+        </div>
+        <div>
+          <p className='text-xs'>Helpful? <span>Yes</span> | <span>Report</span></p>
+        </div>
       </div>
     </div>
   );
