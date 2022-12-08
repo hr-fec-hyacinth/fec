ddb3ac2d2b7be2ee36b0edd58173f0323df61c0d
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-08 09:42:23 -0500

    Added the table of suggested hints

--- src/components/ratingsreviews/CharacteristicInputTable.jsx
+++ src/components/ratingsreviews/CharacteristicInputTable.jsx
@@ -17,23 +17,47 @@ import { useState, useEffect } from 'react'
 
 const CharInputTable = ({metaChars, cb}) => {
   const [tableValues, setTableValues] = useState({});
+  const [hover, setHover] = useState({
+                              active: false,
+                              selected: ''
+                            })
 
   const handleOnChange = (e, name) => {
     cb(e, e.target.name)
   }
 
+  // present an object that changes based on
+  const inputHints = {
+    Size: [null, 'A size too small', '1/2 size too small', 'Perfect', '½ a size too big', 'A size too wide'],
+    Width: [null, 'Too narrow', 'Slightly narrow', 'Perfect', 'Slightly wide', 'Too wide'],
+    Comfort: [null, 'Uncomfortable', 'Slightly uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
+    Quality: [null, 'Poor', 'Below average', 'What I expected', 'Pretty great', 'Perfect'],
+    Length: [null, 'Runs Short', 'Runs slightly short', 'Perfect', 'Runs Slightly Long', 'Runs Long'],
+    Fit: [null, 'Runs tight', 'Runs slightly tight', 'Perfect', 'Run slightly long', 'Runs Long']
+  }
+
   return (<>
     <table className="mx-auto">
+    <tbody>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+    </tbody>
     {Object.keys(metaChars).map((el, i) => {
       return (
-        <tbody className="odd:bg-slate-200 even:bg-slate-100">
-          <tr className="w-full mx-6">
+        <tbody className='odd:bg-slate-200 even:bg-slate-100'>
+          <tr className='w-full mx-6'>
             <td className="pr-8">{el}</td>
             {Array.from({length: 5}, (v, j) => {
               return (
-                <td className="pr-6">
+                <td className='pr-6'>
                   <input
-                    type="radio"
+                    type='radio'
                     name={metaChars[el].id}
                     value={j}
                     onChange={handleOnChange}
