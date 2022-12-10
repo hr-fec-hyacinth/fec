e26f4630773f6dc44c3bb4adc9685d34e3a390f4
Jonathon Zimdars <zimdarsj@kenyon.edu>
2022-12-10 11:41:36 -0600

    Styles Complete

--- src/components/overview/Expanded.jsx
+++ src/components/overview/Expanded.jsx
@@ -37,18 +37,18 @@ const Expanded = ({ updateExpand, styling, imageIndex, imageList, updateStyling,
         updateZoomed(true);
         updateZoomEvent(e);
       }}/>
-        <MdClose className='hover:text-gray-500 text-white right-4 top-4 absolute text-7xl sm:text-2xl cursor-default z-30' onClick={e => { updateExpand(false) }} />
+        <div className='rounded-full bg-white hover:bg-[#6E6E6E] hover:text-white text-[#091E42] right-4 top-4 absolute text-7xl sm:text-2xl cursor-default z-30'><MdClose onClick={e => { updateExpand(false) }} /></div>
 
         {imageIndex !== 0 && <div className='z-10 left-0 sm:hidden min-h-full w-1/12 absolute flex flex-col justify-center text-2xl text-center' onClick={previous}></div>}
-        {imageIndex !== 0 && <BsFillArrowLeftCircleFill data-testid="expanded-left" className='hidden sm:block hover:text-gray-500 text-2xl text-white top-1/2 left-24 absolute z-20 cursor-default' onClick={previous} />}
+        {imageIndex !== 0 && <BsFillArrowLeftCircleFill data-testid="expanded-left" className='hidden rounded-full bg-white sm:block border-white border-2 text-[#1E54B1] hover:text-[#091E42] text-2xl top-1/2 left-24 absolute z-20 cursor-default' onClick={previous} />}
 
         {imageIndex !== imageList.length - 1 && <div className='z-10 sm:hidden right-0 min-h-full w-1/12 absolute flex flex-col justify-center text-2xl text-center' onClick={next}></div>}
-        {imageIndex !== imageList.length - 1 && <BsFillArrowRightCircleFill data-testid="expanded-right" className='hidden sm:block hover:text-gray-500 text-2xl text-white top-1/2 right-24 absolute z-10 cursor-default' onClick={next} />}
+        {imageIndex !== imageList.length - 1 && <BsFillArrowRightCircleFill data-testid="expanded-right" className='rounded-full bg-white hidden sm:block border-white border-2 hover:text-[#091E42] text-[#1E54B1] text-2xl top-1/2 right-24 absolute z-10 cursor-default' onClick={next} />}
 
         <div className='absolute bottom-4 flex'>
           {imageList.map((image, i) => {
             if (i === imageIndex) {
-              return (<VscCircleLargeFilled key={i} data-testid="image-icon" className='sm:text-base text-2xl rounded-full text-blue-400 border-2 border-white mx-1 cursor-default' />);
+              return (<VscCircleLargeFilled key={i} data-testid="image-icon" className='sm:text-base text-2xl rounded-full text-[#1E54B1] border-2 border-white mx-1 cursor-default' />);
             }
             return (<VscCircleLargeFilled key={i} data-testid="image-icon" className='sm:text-base text-2xl text-white mx-1 cursor-default' onClick={e => {
               updateImageIndex(i);
